73e0c5fb70c16fd0d8166981129d9aa10f61b322
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2018-12-25 22:17:01 +0200

    add jwt authentication

---
+++ books store/book store backend/Auth/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace books
+{
+    public class Person
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer"; // издатель токена
+        public const string AUDIENCE = "http://localhost:51884/"; // потребитель токена
+        const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
+        public const int LIFETIME = 20; // время жизни токена - 1 минута
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
---
+++ books store/book store backend/Controllers/AuthController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Newtonsoft.Json;
+using Microsoft.IdentityModel.Tokens;
+using System.Linq;
+
+namespace books
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private List<Person> people = new List<Person>
+        {
+            new Person {Login="admin@gmail.com", Password="12345" },
+            new Person { Login="qwerty", Password="55555", }
+        };
+
+        [HttpPost("/token")]
+        public async Task Token()
+        {
+            var username = Request.Form["username"];
+            var password = Request.Form["password"];
+
+            var identity = GetIdentity(username, password);
+            if (identity == null)
+            {
+                Response.StatusCode = 400;
+                await Response.WriteAsync("Invalid username or password.");
+                return;
+            }
+
+            var now = DateTime.UtcNow;
+
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                         AuthOptions.ISSUER,
+                         AuthOptions.AUDIENCE,
+                         notBefore: now,
+                         claims: identity.Claims,
+                         expires: now.Add(
+                             TimeSpan.FromMinutes(AuthOptions.LIFETIME)
+                         ),
+                         signingCredentials: new SigningCredentials(
+                             AuthOptions.GetSymmetricSecurityKey(), 
+                             SecurityAlgorithms.HmacSha256)
+                         );
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+
+            // сериализация ответа
+            Response.ContentType = "application/json";
+
+            await Response.WriteAsync(
+                JsonConvert.SerializeObject(new
+                {
+                    access_token = encodedJwt,
+                    username = identity.Name
+                },
+                new JsonSerializerSettings { Formatting = Formatting.Indented })
+            );
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            Person person = people.FirstOrDefault(x => x.Login == username && x.Password == password);
+
+            if (person != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login), 
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            // если пользователя не найдено
+            return null;
+        }
+    }
+}
